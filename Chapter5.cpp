9e5394e9c02525bc08e781deb4094c50a5aecd07
ben.li <ben.li@rightcrowd.com>
2022-09-25 11:50:29 +1000

    Add Chapter 5

--- Chapter5.cpp
+++ Chapter5.cpp
@@ -19,17 +19,33 @@ AI remove object from picture:
 auto drow the missing part
 */
 
-
+float w = 250, h = 350;
+Mat matrix, imgWarp;
 // warp
 void main()
 {
 
-    string path = "Resources/cards.png";
-
-    /// read image from opencv type
+    string path = "Resources/cards.jpg";
     Mat img = imread(path);
+
+    // top left, top right, butom left, buttom right
+    Point2f src[4] = { {529,142}, {771, 190}, {405, 395}, {674, 457} };
+    Point2f dst[4] = { {0.0f,0.0f}, {w, 0.0f}, {0.0f, h}, {w, h} };
     
+    // transform matrix
+    matrix = getPerspectiveTransform(src, dst);
+
+    // apply matrix to img, with size
+    warpPerspective(img, imgWarp, matrix, Point(w, h));
+
+
+    for (int i = 0; i < 4; i++)
+    {
+        circle(img, Point(src[i]), 5, Scalar(0, 0, 255), FILLED);
+
+    }
     
     imshow("Image", img);
+    imshow("ImageW", imgWarp);
     waitKey(0);
 }
